a0cda3cebba28c5df8f0b7eca419ecea5f141152
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-06-23 10:35:49 +0200

    Move code tests in `it.unipr.frontend` package

--- rust-lisa/src/test/java/it/unipr/frontend/DummyCFGTest.java
+++ rust-lisa/src/test/java/it/unipr/frontend/DummyCFGTest.java
@@ -1,14 +1,15 @@
-import org.junit.Test;
+package it.unipr.frontend;
 
 import it.unive.lisa.AnalysisSetupException;
 import it.unive.lisa.LiSAConfiguration;
+import org.junit.Test;
 
-public class DummyCFGTest extends RustLiSATestExecutor {
+public class DummyCFGTest
+		extends RustLiSATestExecutor {
 
 	@Test
 	public void dummyTest() throws AnalysisSetupException {
 		LiSAConfiguration conf = new LiSAConfiguration().setDumpCFGs(true).setJsonOutput(true);
 		perform("dummy", "example.rs", conf);
 	}
-	
 }
