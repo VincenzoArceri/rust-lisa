e8812f5643b21c830778cd4e70a81d3b34fc2580
Simone Gazza <gazza.sim@gmail.com>
2022-06-24 16:03:15 +0200

    #2 Add test for multiple ifs

---
+++ rust-lisa/rust-testcases/cfg/multiple-if/multiple-if.rs
@@ -0,0 +1,16 @@
+fn main() {
+    if x == 5 {
+        x = 1;
+    } else if x == 1 {
+        x = 2;
+    } else if x == 2 {
+        x = 3;
+    } else if x == 3 {
+        x = 4;
+    } else if x == 4 {
+        x = 5;
+    } else {
+        x = 6;
+    }
+}
+
