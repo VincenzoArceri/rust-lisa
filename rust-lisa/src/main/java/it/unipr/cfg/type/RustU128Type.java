904bb6defb9e5a24ef5f247c41c62e07dcebf8c9
Simone Gazza <gazza.sim@gmail.com>
2022-06-28 10:14:36 +0200

    #1 Add rust primitive signed integer type

---
+++ rust-lisa/src/main/java/it/unipr/cfg/type/RustU128Type.java
@@ -0,0 +1,77 @@
+package it.unipr.cfg.type;
+
+import it.unive.lisa.type.NumericType;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Unique instance of the Rust u128 type.
+ *
+ * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
+ * @author <a href="mailto:simone.gazza@studenti.unipr.it">Simone Gazza</a>
+ * 
+ * @TODO LiSA does not support 128 bits type. So no method isXXBits will be
+ *           true.
+ */
+public class RustU128Type implements NumericType {
+
+	/**
+	 * Unique instance of Rust u128 type.
+	 */
+	public static final RustU128Type INSTANCE = new RustU128Type();
+
+	private RustU128Type() {
+	}
+
+	@Override
+	public boolean canBeAssignedTo(Type other) {
+		return other instanceof RustU128Type || other instanceof Untyped;
+	}
+
+	@Override
+	public Type commonSupertype(Type other) {
+		// Rust cast ought to be explicit by design
+		// https://doc.rust-lang.org/rust-by-example/types/cast.html
+		if (other instanceof RustU128Type)
+			return other;
+		return Untyped.INSTANCE;
+	}
+
+	@Override
+	public Collection<Type> allInstances() {
+		return Collections.singleton(INSTANCE);
+	}
+
+	@Override
+	public boolean is8Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean is16Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean is32Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean is64Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean isUnsigned() {
+		return true;
+	}
+
+	@Override
+	public boolean isIntegral() {
+		return true;
+	}
+
+}
