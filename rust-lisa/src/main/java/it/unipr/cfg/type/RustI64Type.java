2204a289ff98d32c9204071dc4f1f865ddb25284
Simone Gazza <gazza.sim@gmail.com>
2022-06-28 10:15:14 +0200

    #1 Add rust primitive signed integer type

---
+++ rust-lisa/src/main/java/it/unipr/cfg/type/RustI64Type.java
@@ -0,0 +1,74 @@
+package it.unipr.cfg.type;
+
+import it.unive.lisa.type.NumericType;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Unique instance of the Rust i64 type.
+ *
+ * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
+ * @author <a href="mailto:simone.gazza@studenti.unipr.it">Simone Gazza</a>
+ */
+public class RustI64Type implements NumericType {
+
+	/**
+	 * Unique instance of Rust i64 type.
+	 */
+	public static final RustI64Type INSTANCE = new RustI64Type();
+
+	private RustI64Type() {
+	}
+
+	@Override
+	public boolean canBeAssignedTo(Type other) {
+		return other instanceof RustI64Type || other instanceof Untyped;
+	}
+
+	@Override
+	public Type commonSupertype(Type other) {
+		// Rust cast ought to be explicit by design
+		// https://doc.rust-lang.org/rust-by-example/types/cast.html
+		if (other instanceof RustI64Type)
+			return other;
+		return Untyped.INSTANCE;
+	}
+
+	@Override
+	public Collection<Type> allInstances() {
+		return Collections.singleton(INSTANCE);
+	}
+
+	@Override
+	public boolean is8Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean is16Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean is32Bits() {
+		return false;
+	}
+
+	@Override
+	public boolean is64Bits() {
+		return true;
+	}
+
+	@Override
+	public boolean isUnsigned() {
+		return false;
+	}
+
+	@Override
+	public boolean isIntegral() {
+		return true;
+	}
+
+}
