abb3844301d57a09edb20b88726b99240a055021
Simone Gazza <gazza.sim@gmail.com>
2022-07-08 16:09:54 +0200

    #22 Add RustUnitLiteral

---
+++ rust-lisa/src/main/java/it/unipr/cfg/expression/literal/RustUnitLiteral.java
@@ -0,0 +1,27 @@
+package it.unipr.cfg.expression.literal;
+
+import it.unipr.cfg.type.RustUnitType;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.literal.Literal;
+
+/**
+ * Rust unit literal.
+ * 
+ * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
+ * @author <a href="mailto:simone.gazza@studenti.unipr.it">Simone Gazza</a>
+ */
+public class RustUnitLiteral extends Literal<Void> {
+
+	/**
+	 * Build the unit literal.
+	 * 
+	 * @param cfg      the {@link CFG} where this literal lies
+	 * @param location the location where this literal is defined
+	 * @param value    the char value
+	 */
+	public RustUnitLiteral(CFG cfg, CodeLocation location) {
+		super(cfg, location, null, RustUnitType.INSTANCE);
+	}
+
+}
