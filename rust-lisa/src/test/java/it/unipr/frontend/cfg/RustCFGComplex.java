741b78f703c96d9d6ed9c8b2a1adb152d80710fb
Simone Gazza <gazza.sim@gmail.com>
2022-07-11 09:58:02 +0200

    #29 Add test for methods

---
+++ rust-lisa/src/test/java/it/unipr/frontend/cfg/RustCFGComplex.java
@@ -0,0 +1,16 @@
+package it.unipr.frontend.cfg;
+
+import it.unipr.frontend.RustLiSATestExecutor;
+import it.unive.lisa.AnalysisSetupException;
+import it.unive.lisa.LiSAConfiguration;
+import org.junit.Test;
+
+public class RustCFGComplex extends RustLiSATestExecutor {
+
+	@Test
+	public void testRectangle() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration().setDumpCFGs(true).setJsonOutput(true);
+		perform("cfg/rectangle", "rectangle.rs", conf);
+	}
+	
+}
