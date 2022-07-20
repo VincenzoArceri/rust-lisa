8f91c2edfb91e76e46e723e047567bac51655b19
Simone Gazza <gazza.sim@gmail.com>
2022-07-20 11:00:15 +0200

    #32 strengthen if-let test

--- rust-lisa/rust-testcases/cfg/simple-if-let/simple-if-let.rs
+++ rust-lisa/rust-testcases/cfg/simple-if-let/simple-if-let.rs
@@ -1,16 +1,22 @@
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
-    Write(&str),
     ChangeColor(i32, i32, i32),
 }
 
 fn main() {
-    let m = Message::ChangeColor(0, 0, 0);
 
-    if let Message::ChangeColor(a, b, c) = m {
+    
+    let m1 = Message::ChangeColor(0, 0, 0);
+    if let Message::ChangeColor(a, b, c) = m1 {
         println!("{}", a);
-    } else {
-        return;
+        println!("{}", b);
+        println!("{}", c);
+    }
+    
+    let m2 = Message::Move {x : 1, y : 2};
+    if let Message::Move{ x: a, y: b} = m2 {
+        println!("{}", a);
+        println!("{}", b);
     }
 }
